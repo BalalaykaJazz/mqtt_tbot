f8bc14bb1e3ae7977907c694db65f75bf211c231
Balalaykajazz <archer2000@mail.ru>
2021-04-26 00:08:21 +0300

    fix tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,25 +1,29 @@
+"""Тестируется файл config.py"""
 import pytest
 from pod_mqtt_tbot import config
 
 
 def test_load_settings():
+    """При считывании настроек должен возвращаться словарь с обязательным набором ключей"""
+
     expected = {"host": "localhost", "port": 80, "name": "unknown", "tg_token": "123"}
     returned_value = config.load_settings()
 
     assert expected.keys() == returned_value.keys()
 
 
-def test_get_settings_correct_setting():
-    incorrect_settings = {}
-    incorrect_parameter = ""
+@pytest.mark.parametrize('test_example', [{}, ""])
+def test_get_settings_correct_setting(test_example):
+    """Если настройки считаны некорректно, то функция get_settings должна бросать исключения"""
 
     with pytest.raises(config.SettingsError):
-        config.get_settings(incorrect_settings, "unknown")
-        config.get_settings(incorrect_parameter, "unknown")
+        config.get_settings(test_example, "unknown")
 
 
 def test_get_settings_correct_return():
-    expected = {"host": "localhost"}
+    """Функция должна возвращать строку"""
+
+    expected = {"host": "localhost", "name": "test"}
 
     returned_value = config.get_settings(expected, "host")
     assert returned_value == "localhost"
--- tests/test_mqtt_tbot_run.py
+++ tests/test_mqtt_tbot_run.py
@@ -1,20 +1,21 @@
+"""Тестируется файл mqtt_tbot_run.py"""
 import pytest
-from pod_mqtt_tbot import mqtt_tbot_run
+from pod_mqtt_tbot import mqtt_tbot_run  # type: ignore
 
 
-def test_check_message_exception():
-    # Json format
-    with pytest.raises(mqtt_tbot_run.FormatError):
-        mqtt_tbot_run.check_message("test")
-
-    with pytest.raises(mqtt_tbot_run.FormatError):
-        mqtt_tbot_run.check_message(42)
+@pytest.mark.parametrize('test_example', ["test",
+                                          42,
+                                          '{"without_topic": "test", "message": "test"}'])
+def test_check_message_exception(test_example):
+    """Если полученное сообщение имеет некорректный формат, то функция должна бросать исключения"""
 
     with pytest.raises(mqtt_tbot_run.FormatError):
-        mqtt_tbot_run.check_message('{"without_topic": "test", "message": "test"}')
+        mqtt_tbot_run.check_message(test_example)
 
 
 def test_check_message_return():
+    """Если полученное сообщение имеет корректный формат, то функция должна возвращать истину"""
+
     correct = '{"topic": "test", "message": "test"}'
     returned_value = mqtt_tbot_run.check_message(correct)
     assert returned_value is True
