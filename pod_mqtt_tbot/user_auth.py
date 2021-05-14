aa10cd8350743185637ecff10a4c3ec1c0d09cf4
Balalaykajazz <archer2000@mail.ru>
2021-05-14 17:37:07 +0300

    change code format

--- pod_mqtt_tbot/user_auth.py
+++ pod_mqtt_tbot/user_auth.py
@@ -5,15 +5,16 @@
 import hashlib
 import os
 
+CODE = "latin_1"
+
 
 def encode_password(client_password: str) -> str:
     """
     Пароль пользователя хешируется и отправляется в mqtt_publisher
     """
 
-    # salt = os.urandom(32)
-    salt = b'8\xbaU\x19\xb9\xca\xf6\xdb`Q\xb4\x88]\xdc\x81M\xbb\xd3\xbd%\x19\xdd\x80\xae\xb1\xf8\x01\x03\x92\x11\xb5Z'
-    key = hashlib.pbkdf2_hmac("sha256", client_password.encode("UTF-8"), salt, 100000)
+    salt = os.urandom(32)
+    key = hashlib.pbkdf2_hmac("sha256", client_password.encode(CODE), salt, 100000)
     token = salt + key
 
-    return token.decode("cp1251")
+    return token.decode(CODE)
