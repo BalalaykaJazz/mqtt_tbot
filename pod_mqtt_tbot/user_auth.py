a5e5f402e2575b26fd27b374dc5b6ba4590b75f0
Balalaykajazz <archer2000@mail.ru>
2021-05-18 00:03:39 +0300

    upd auth

--- pod_mqtt_tbot/user_auth.py
+++ pod_mqtt_tbot/user_auth.py
@@ -3,20 +3,16 @@
 """
 
 import hashlib
-import base64
-
-CODE = "ascii"
 
 
-def encode_password(client_password: str, salt_b64: str) -> str:
+def encode_password(client_password: str, salt_hash: str) -> str:
     """
     Пароль пользователя хешируется и отправляется в mqtt_publisher
     """
 
-    salt = base64.b64decode(salt_b64)
-    password_hash = hashlib.pbkdf2_hmac("sha256", client_password.encode(CODE), salt, 100000)
-    token_hash = salt + password_hash
+    password_hash = hashlib.pbkdf2_hmac("sha256",
+                                        client_password.encode(),
+                                        salt_hash.encode(),
+                                        100000)
 
-    token = base64.b64encode(token_hash).decode(CODE)
-
-    return token
+    return salt_hash + password_hash.hex()
