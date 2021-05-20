22254becbf1ce9e5f8c29504fb950adf9787ce8d
Balalaykajazz <archer2000@mail.ru>
2021-05-20 15:13:52 +0300

    upd test

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,30 +3,47 @@ import pytest
 from pod_mqtt_tbot import config  # type: ignore
 
 
+def test_read_file():
+    """Проверка корректности чтения файла"""
+
+    with pytest.raises(config.SettingsError):
+        config.read_file("no_file")
+
+    returned_value = config.read_file(config.MAIN_SETTINGS_PATH)
+
+    assert isinstance(returned_value, dict)
+
+
 def test_load_settings():
     """При считывании настроек должен возвращаться словарь с обязательным набором ключей"""
 
     expected = {"host": "localhost", "port": 80,
                 "name": "unknown", "tg_token": "123",
                 "topic_templates": [],
-                "use_ssl": False}
+                "use_ssl": False,
+                "correctly": True}
+
     returned_value = config.load_settings()
 
     assert expected.keys() == returned_value.keys()
 
 
 @pytest.mark.parametrize('test_example', [{}, ""])
-def test_get_settings_correct_setting(test_example):
+def test_get_settings_exc(test_example):
     """Если настройки считаны некорректно, то функция get_settings должна бросать исключения"""
 
+    _settings = config.load_settings()
+
     with pytest.raises(config.SettingsError):
-        config.get_settings(test_example, "unknown")
+        config.get_settings("unknown")
 
 
 def test_get_settings_correct_return():
-    """Функция должна возвращать строку"""
+    """Функция должна возвращать корректные значения"""
 
-    expected = {"host": "localhost", "name": "test"}
-
-    returned_value = config.get_settings(expected, "host")
+    _settings = {"host": "localhost", "port": 80}
+    returned_value = config.get_settings("host")
     assert returned_value == "localhost"
+
+    returned_value = config.get_settings("socket")
+    assert isinstance(returned_value, tuple)
