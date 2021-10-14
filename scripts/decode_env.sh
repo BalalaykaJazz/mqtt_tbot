5982855b24f3f3e464323c15c38ccffd4950ca7b
Balalaykajazz <archer2000@mail.ru>
2021-10-14 11:44:31 +0300

    Исправлен скрипт для декодирования настроек подключения.

--- scripts/decode_env.sh
+++ scripts/decode_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Декодирование настроек
 
+scp ../src/mqtt_tbot/settings/enc_env ../src/mqtt_tbot/settings/.env
 sops --hc-vault-transit $VAULT_ADDR/v1/sops/keys/iot  --verbose -d -i ../src/mqtt_tbot/settings/.env
